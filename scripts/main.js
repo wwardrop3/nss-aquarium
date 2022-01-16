5279c9db630143099533bc270fb8c9281d40e147
Wil Wardrop <wwardrop@gmail.com>
2022-01-16 17:52:48 -0600

    added main js file

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const database = {
+    fish: [
+        {
+            name: "Carl",
+            size: "Large",
+            food: "crabs",
+        }
+    ]
+}
+
+console.log(database(fish).name)
