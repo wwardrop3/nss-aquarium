9cab38962b3f240fc6fee55e45fe580ac4ae0e60
Wil Wardrop <wwardrop@gmail.com>
2022-02-15 15:37:33 -0600

    Pioneer done

--- scripts/database.js
+++ scripts/database.js
@@ -1,9 +1,3 @@
-//martins aquarium database file
-export const getFish = () => {
-    return database.fish.map(fish => ({...fish}))
-}
-
-
 const database = {
     fish: [
         {
@@ -12,7 +6,7 @@ const database = {
             image: "https://www.aquariumofpacific.org/images/made_new/images-uploads-clownfish_400_q85.jpg",
             species: "Clownfish",
             length: "3",
-            location: "Large",
+            location: "Australia",
             food: "Meat",
 
         },
@@ -22,7 +16,7 @@ const database = {
             image: "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2VlbHMtc2xpcHBlcnktMi5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjI5MH19fQ==",
             species: "Eel",
             length: "5",
-            location: "Large",
+            location: "Chicago",
             food: "Other Fish",
         },
         {
@@ -31,20 +25,34 @@ const database = {
             image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimagesvc.meredithcorp.io%2Fv3%2Fmm%2Fimage%3Furl%3Dhttps%253A%252F%252Fstatic.onecms.io%252Fwp-content%252Fuploads%252Fsites%252F6%252F2016%252F03%252Ffinding-nemo.jpg&q=85",
             species: "Great White",
             length: "151",
-            location: "Large",
+            location: "Ocean",
             food: "Vegitarian",
+        },
+    ],
+    tips: ["Wear a wetsuit", "Don't dive too deep", "Don't feed sharks"],
+    locations:[
+        {
+            name: "Australia",
+            image: "https://cdn.pixabay.com/photo/2016/03/31/21/55/australia-1296727__340.png",
+        },
+        {
+            name: "Meditaranian",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Mediterranee_02_EN.jpg/264px-Mediterranee_02_EN.jpg",
+        },
+        {
+            name: "Ocean",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Phycodurus_eques_range_map.png/220px-Phycodurus_eques_range_map.png",
         }
-
     ]
 }
 
-const fishes = getFish()
+
 
 export const mostHolyFish = () => {
     // 3, 6, 9, 12, etc... fish
     const holyFish = []
 
-    for (const fish of fishes) {
+    for (const fish of getFish()) {
         if (fish.length % 3 === 0){
             fish.type = "Holy Fish"
             holyFish.push(fish)
@@ -55,11 +63,16 @@ export const mostHolyFish = () => {
 }
 
 
+//martins aquarium database file
+export const getFish = () => {
+    return database.fish.map(fish => ({...fish}))
+}
+
 export const soldierFish = () => {
 // 5, 10, 15, 20, 25, etc... fish
     const soldiers = []
 
-    for (const fish of fishes) {
+    for (const fish of getFish()) {
         if (fish.length % 5 === 0) {
             fish.type = "Soldier Fish"
             soldiers.push(fish)
@@ -72,7 +85,7 @@ export const soldierFish = () => {
 export const nonHolyFish = () => {
 // Any fish not a multiple of 3 or 5
     const regularFish = []
-    for (const fish of fishes) {
+    for (const fish of getFish()) {
         if (fish.length % 3 !== 0 && fish.length % 5 !== 0) {
             fish.type = "Unholy Fish"
             regularFish.push(fish)
@@ -82,4 +95,11 @@ export const nonHolyFish = () => {
     return regularFish
 }
 
+export const getTips = () => {
+    return database.tips
+}
 
+export const getLocations = () => {
+    const listArray = database.locations.map(location => ({...location}))
+    return listArray
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,80 @@
 import { getFish } from "./database.js"
-import { FishList } from "./fishList.js"
-
-const parentHTMLElement = document.querySelector("#fishSection")
-
-parentHTMLElement.innerHTML = FishList()
+import { renderTips } from "./tips.js"
+import { FishList } from "./fish.js"
+import { renderLocations } from "./locations.js"
 
 
-const allFish = getFish()
 
-for (const fish of allFish) {
-    console.log(fish)
+
+const renderPage = () => {
+    return `
+    <article class = "page">
+        <section class = "navBar">
+            <section class="pageTitle">
+                <h1><b>Martin's Aquarium</b</h1>
+            </section>
+            <section class = "menuButtons">
+                <button>Future Page</button>
+            </section>
+        </section>
+
+        <section class = "content">
+            <section class = "sections" id = "fishSection">
+                ${FishList()}
+    
+            </section>
+    
+            <section class = 'sections'>
+                <section class = "subsections">
+                    <h1 class = "h1">Travel Tips</h1>
+                    <section class = "lists">
+                        ${renderTips()}
+                    </section>
+                    
+                </section>
+            <section class = "subsections">
+                <h1>Harvest Locations</h1>
+                <section class = "lists">
+                    ${renderLocations()}              
+                </section>
+                
+
+            </section>
+    
+            </section>
+
+            <section class = "sections">
+                <section class = "subsections" id="tank-cleaning">
+                    <h1 class = "h1">Tank Cleaning Instructions</h1>
+                    <section class = "lists">
+                        <ul>
+                            <li>Salinity: Medium</li>
+                            <li>Temperature: 70 Degrees</li>
+                            <li>Tank Cleaning Instruction: Make sure Nemo doesn't get stuck in the machine</li>
+                        </ul>
+
+                    </section>
+
+
+                </section>
+            </section>
+    
+        </section>
+        <section class = "footer">
+            <section class = "footerText">
+                <p>Footer</p>
+                <p>Address</p>
+            </section>
+        
+        </section>  
+
+
+
+
+        </section>
+    </article>`
+
 }
 
-
+document.querySelector("#contentContainer").innerHTML = renderPage()
 
